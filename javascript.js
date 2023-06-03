8372bc69b6e081876fbc042ddc96fc2f9fe52d31
eevb <96337431+eevb@users.noreply.github.com>
2023-06-03 12:25:47 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -10,12 +10,20 @@ function checkHiddenElement() {
   
   if (!existingElement) {
     var newElement = document.createElement('div');
-    var randomClass = '63-' + Math.floor(Math.random() * 1000); // Change the range as needed
+    var randomClass = '63-' + Math.floor(Math.random() * 1000);
     
     newElement.className = randomClass;
     newElement.textContent = 'block me with ublock origin';
     document.body.appendChild(newElement);
     existingElement = newElement;
+    
+    // Add an event listener to detect changes in the element
+    newElement.addEventListener('DOMSubtreeModified', function() {
+      // If the element is modified, add a new element
+      if (existingElement === newElement) {
+        existingElement = null; // Reset the existing element variable
+      }
+    });
   }
 }
 
